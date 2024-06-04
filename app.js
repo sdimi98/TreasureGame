5226f92d26b5b4d07caa109a64a0eef287960019
sdimi98 <Sup3rrXDA1@gmail.com>
2024-06-05 02:04:23 +0300

    Application initialized with canvas fit to screen. Background loaded in and fit to screen.

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 Start();
 
-async function Start()
-{
+async function Start() {
     const app = new PIXI.Application();
-    await app.init({ width: 640, height: 360 });
+    await app.init({
+        transparent: false,
+        antialias: true
+    });
+    app.renderer.resize(window.innerWidth, window.innerHeight);
     document.body.appendChild(app.canvas);
+    await PIXI.Assets.load('./assets/bg.png');
+    let sprite = PIXI.Sprite.from('./assets/bg.png');
+    sprite.height = window.innerHeight;
+    sprite.width = window.innerWidth;
+    app.stage.addChild(sprite);
+    app.renderer.view.canvas.style.position = 'absolute';
+
 }

7bc2f60de937e08f60028c80dd0cc47a0d2163af
sdimi98 <Sup3rrXDA1@gmail.com>
2024-06-06 01:42:06 +0300

    Handle rotation animation implemented.

--- app.js
+++ app.js
@@ -25,17 +25,58 @@ async function Start() {
     doorSprite.position.set(app.renderer.width / 2 + 18, app.renderer.height / 2 - 12);
     app.stage.addChild(doorSprite);
 
-    //Handle Shadow
+
+    //handleShadow
     let handleShadowSprite = await createSprite('./assets/handleShadow.png', 0.5, 0.5, 0.31);
     handleShadowSprite.position.set(doorSprite.position.x - 22, doorSprite.position.y + 6);
     app.stage.addChild(handleShadowSprite);
 
 
-    //Handle
+    //handleSprite
     let handleSprite = await createSprite('./assets/handle.png', 0.5, 0.5, 0.31);
     handleSprite.position.set(doorSprite.position.x - 27, doorSprite.position.y - 5);
     app.stage.addChild(handleSprite);
-    app.renderer.view.canvas.style.position = 'absolute';
 
+    // leftArrowSprite
+    let leftArrowSprite = await createSprite('./assets/arrowLeft.png', 0.5, 0.5, 0.31);
+    leftArrowSprite.position.set(doorSprite.position.x - 150, doorSprite.position.y - 150);
+    leftArrowSprite.alpha = 0.5;
+    leftArrowSprite.interactive = true;
+    leftArrowSprite.cursor = 'pointer';
+    leftArrowSprite.on('pointerdown', () => rotateHandle('left'));
+    app.stage.addChild(leftArrowSprite);
+
+    // rightArrowSprite
+    let rightArrowSprite = await createSprite('./assets/arrowRight.png', 0.5, 0.5, 0.31);
+    rightArrowSprite.position.set(doorSprite.position.x + 95, doorSprite.position.y - 150);
+    rightArrowSprite.alpha = 0.5;
+    rightArrowSprite.interactive = true;
+    rightArrowSprite.cursor = 'pointer';
+    rightArrowSprite.on('pointerdown', () => rotateHandle('right'));
+    app.stage.addChild(rightArrowSprite);
+
+    function rotateHandle(direction) {
+        if (direction === 'left') {
+            gsap.to(handleSprite, {
+                duration: 0.5,
+                rotation: handleSprite.rotation - 1.0472
+            });
+            gsap.to(handleShadowSprite, {
+                duration: 0.5,
+                rotation: handleShadowSprite.rotation - 1.0472
+            });
+        } else if (direction === 'right') {
+            gsap.to(handleSprite, {
+                duration: 0.5,
+                rotation: handleSprite.rotation + 1.0472
+            });
+            gsap.to(handleShadowSprite, {
+                duration: 0.5,
+                rotation: handleShadowSprite.rotation + 1.0472
+            });
+        }
+    }
+
+    app.renderer.view.canvas.style.position = 'absolute';
 
 }

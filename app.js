008a3fc8be500bf0ce587a2a7f51587c768cd649
sdimi98 <Sup3rrXDA1@gmail.com>
2024-06-05 19:49:43 +0300

    Placed the door. Centered and scaled onto the opening.

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ async function Start() {
     });
     app.renderer.resize(window.innerWidth, window.innerHeight);
     document.body.appendChild(app.canvas);
+
     await PIXI.Assets.load('./assets/bg.png');
-    let sprite = PIXI.Sprite.from('./assets/bg.png');
-    sprite.height = window.innerHeight;
-    sprite.width = window.innerWidth;
-    app.stage.addChild(sprite);
+    let bgSprite = PIXI.Sprite.from('./assets/bg.png');
+    bgSprite.height = window.innerHeight;
+    bgSprite.width = window.innerWidth;
+    app.stage.addChild(bgSprite);
+    app.renderer.view.canvas.style.position = 'absolute';
+
+    await PIXI.Assets.load('./assets/door.png');
+    let doorSprite = PIXI.Sprite.from('./assets/door.png');
+    doorSprite.scale.set(0.31,0.31);
+    doorSprite.position.x = (bgSprite.width / 2) - (doorSprite.width / 2) + 18;
+    doorSprite.position.y = (bgSprite.height / 2) - (doorSprite.height / 2) - 12;
+    app.stage.addChild(doorSprite);
     app.renderer.view.canvas.style.position = 'absolute';
 
 }
